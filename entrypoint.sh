00ec77b530fd64456cb0e9d76c3bc5df528c3315
ine1127 <st_bluesunburst@yahoo.co.jp>
2017-05-31 23:12:30 +0900

    add 'init' argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,17 @@ source ${LDAP_RUNTIME_DIR}/functions
 
 if [ ! -z $1 ]; then
   case $1 in
-    start )
+    start|init )
       __slapd_load_domain
       __slapd_install
       __slapd_configure
-      __slapd_start
+
+      case $1 in
+        start )
+          __slapd_start
+        ;;
+      esac
+
     ;;
     stop )
       __slapd_stop

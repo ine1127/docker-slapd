d48861936c91ed176e0ab05b83309bb25063bb07
ine1127 <st_bluesunburst@yahoo.co.jp>
2018-01-18 19:11:18 +0900

    added function __usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,33 @@
 
 source ${LDAP_RUNTIME_DIR}/functions
 
+function __usage() {
+  cat <<EOL
+$(basename ${0}) is a script for starting slapd service in Docker container
+
+Usage:
+    $(basename ${0}) [[command] [arguments]]
+
+Command:
+    init            initialize settings
+    start           init and start slapd
+    stop            stop slapd
+    status          check status
+    backup_config   OpenLDAP config backup (using slapcat)
+    backup_dbdata   OpenLDAP Database backup
+    exec            execute linux command on Docker container
+                    required [arguments]
+
+    ex)
+        $ $(basename ${0}) exec ls -la
+EOL
+}
+
 if [ ! -z $1 ]; then
   case $1 in
+    --help|-h|help|--usage|usage )
+      __usage
+    ;;
     start|init )
       __slapd_load_domain
       __slapd_deploy
@@ -33,4 +58,7 @@ if [ ! -z $1 ]; then
       exec $@
     ;;
   esac
+else
+  __usage
+  exit 1
 fi
